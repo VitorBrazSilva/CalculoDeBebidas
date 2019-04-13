6df762c1a45762a186df30a59068edacdcc9f45d
Vitor Braz <vitorbraz_@hotmail.com>
2019-04-13 03:13:06 -0300

    Requisição feita com sucesso

--- src/screens/login.js
+++ src/screens/login.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
-import { View, Text, Button, AsyncStorage } from 'react-native'
+import { View, Text, Button, AsyncStorage } from 'react-native';
+
 
 import api from './../services/api';
-
 
 
 export default class Login extends Component {
@@ -10,32 +10,35 @@ export default class Login extends Component {
         erroMessage: null,
     }
     signIn = async () => {
-   try{
+      try{
+   
+          const response = await api.get('https://ticket4you.com.br/web/Api/Controller/Comprador/login.php', {
+          functionPage: 'LoginPage',
+          email: 'vitor@sivis.com.br',
+          senha: 'T4y2010!',
+          });            
 
-            const response = await api.get('Api/Controller/Comprador/login.php?functionPage=LoginPage', {
-            functionPage: 'LoginPage',
-            email: '',
-            senha: '',
-            });            
-        
-            const { user } = response.data;            
-        
-            await AsyncStorage.multiSet([                
-                ['@CodeApi:user', JSON.stringfy(user)],
-            ]);        
-        } catch (response){
-            console.log(response);
-            this.setState({ erroMessage: response.data })
+          if(response.data === 'false'){            
+            throw 'Login ou Senha inválido';
+          } 
+          console.log(response.data)
+          
+          const user  = response.data;   
 
-
+          await AsyncStorage.setItem(               
+              '@CodeApi:user', JSON.stringify(user),
+          );        
+         
+        } catch (response){               
+          this.setState({ erroMessage: response })            
         } 
-      };
-
+         };
+   
   render() {
     const {navigate} = this.props.navigation;
     return (
       <View>
-      { this.state.erroMessage && <Text>{ this.state.erroMessage }</Text> }
+      { this.state.erroMessage && <Text>{ this.state.erroMessage }</Text> }      
        <Button onPress={this.signIn} title="Entrar Login" />
        <Button
         title="Entrar"
